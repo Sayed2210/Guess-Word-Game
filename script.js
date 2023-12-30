f5818fda66f494ae6ca7dfa90e11167c11afc1db
Sayed2210 <eh600039@gmail.com>
2023-12-30 17:03:52 +0200

    create layout

---
+++ script.js
@@ -0,0 +1,7 @@
+// setting game name
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} by Elsayed Hassan ${new Date().getFullYear()}`;
